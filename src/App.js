638c7fed9187b3a234306f58f2490750fc153877
Tovarisch Dawe <andafsul4@gmail.com>
2022-05-11 14:22:29 +0700

    Menambah File CSS
    - Menambah File CSS
    - Mengembangkan File Expense Item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ExpenseItem from './components/ExpenseItem'
 function App() {
   return (
     <div>
     <h2>Lets Get Started</h2>
+    <ExpenseItem></ExpenseItem>
     </div>
   );
 }
