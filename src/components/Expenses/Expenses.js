c3346ba72b0262b551dbe061272a1a953ee694c0
Tovarisch Dawe <andafsul4@gmail.com>
2022-05-11 17:40:57 +0700

    Organizing Folder
    Mengorganisir File

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,16 @@
+import React from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css'
+import Card from '../UI/Card';
 
 function Expenses(props){
 
-    return ( <div className='expenses'>
+    return ( <Card className='expenses'>
     <ExpenseItem title={props.item[0].title} amount={props.item[0].amount} date={props.item[0].date}></ExpenseItem>
     <ExpenseItem title={props.item[1].title} amount={props.item[1].amount} date={props.item[1].date}></ExpenseItem>
     <ExpenseItem title={props.item[2].title} amount={props.item[2].amount} date={props.item[2].date}></ExpenseItem>
     <ExpenseItem title={props.item[3].title} amount={props.item[3].amount} date={props.item[3].date}></ExpenseItem>
-        </div>)
+        </Card>)
 }
 
 export default Expenses;
