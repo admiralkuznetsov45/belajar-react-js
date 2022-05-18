e0ab6825d287ce9a7c3d399ea72307646979b0b1
Tovarisch Dawe <andafsul4@gmail.com>
2022-05-18 23:13:14 +0700

    Update ExpenseItem.js
    ganti kata

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,16 +7,16 @@ const  ExpenseItem = (props) =>{
     const [title,setTitle] = useState(props.title)
 
     const clickHandler = () => {
-        title = "Updated!"
+        setTitle("Updated")
         console.log(title)
     }
 
     return ( 
         <Card className='expense-item'>
         <ExpenseDate date = {props.date}/>
-        <div className='expense-item__description'> <h2>{props.title}</h2></div> 
+        <div className='expense-item__description'> <h2>{title}</h2></div> 
         <div className='expense-item__price'>{props.amount}</div>
-        <button onClick={clickHandler}>Tombol</button>
+        <button onClick={clickHandler}>Tombol Yang Bisa Dipencet</button>
         </Card>
     )
 }
